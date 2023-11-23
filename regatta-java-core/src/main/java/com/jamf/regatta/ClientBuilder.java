fc769b2ea9d8a023aee41de94d8863c2fb5df255
jan.vavro <jan.vavro@jamf.com>
2023-11-23 16:37:23 +0100

    add: configuration of client

--- regatta-java-core/src/main/java/com/jamf/regatta/ClientBuilder.java
+++ regatta-java-core/src/main/java/com/jamf/regatta/ClientBuilder.java
@@ -1,32 +1,35 @@
 package com.jamf.regatta;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.net.ssl.SSLException;
 
 import com.google.common.base.Strings;
 import com.jamf.regatta.impl.ClientImpl;
 
+import io.grpc.ClientInterceptor;
 import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NegotiationType;
 import io.grpc.netty.NettyChannelBuilder;
+import io.netty.channel.ChannelOption;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
 public class ClientBuilder {
 
 	private String target;
+	private boolean insecureSkipTLSVerify;
+	private Duration connectTimeout;
+	private NegotiationType negotiationType;
+	private List<ClientInterceptor> interceptors;
 
 	ClientBuilder() {
 	}
 
 	/**
-	 * Gets the regatta target.
-	 *
-	 * @return the regatta target.
-	 */
-	public String target() {
-		return target;
-	}
-
-	/**
-	 * configure etcd server endpoints.
+	 * configure regatta server endpoint.
 	 *
 	 * @param target regatta server target
 	 * @return this builder to train
@@ -41,6 +44,64 @@ public class ClientBuilder {
 	}
 
 	/**
+	 * Configure if TLS cert verification should be skipped
+	 *
+	 * @return this builder
+	 */
+	public ClientBuilder insecureSkipTLSVerify(boolean insecureSkipTLSVerify) {
+		this.insecureSkipTLSVerify = insecureSkipTLSVerify;
+		return this;
+	}
+
+	/**
+	 * Configure connection timeout.
+	 *
+	 * @param connectTimeout Sets the connection timeout.
+	 * @return this builder
+	 */
+	public ClientBuilder connectTimeout(Duration connectTimeout) {
+		if (connectTimeout != null) {
+			long millis = connectTimeout.toMillis();
+			if ((int) millis != millis) {
+				throw new IllegalArgumentException("connectTimeout outside of its bounds, max value: " +
+						Integer.MAX_VALUE);
+			}
+		}
+		this.connectTimeout = connectTimeout;
+		return this;
+	}
+
+	/**
+	 * Configure negotiationType
+	 *
+	 * @param negotiationType negotiationType (TLS / plaintext)
+	 * @return this builder
+	 */
+	public ClientBuilder negotiationType(NegotiationType negotiationType) {
+		this.negotiationType = negotiationType;
+
+		return this;
+	}
+
+	/**
+	 * Add an interceptor(s).
+	 *
+	 * @param interceptor  an interceptors to add
+	 * @param interceptors additional interceptors
+	 * @return this builder
+	 */
+	public ClientBuilder interceptor(ClientInterceptor interceptor, ClientInterceptor... interceptors) {
+		if (this.interceptors == null) {
+			this.interceptors = new ArrayList<>();
+		}
+
+		this.interceptors.add(interceptor);
+		this.interceptors.addAll(Arrays.asList(interceptors));
+
+		return this;
+	}
+
+	/**
 	 * build a new Client.
 	 *
 	 * @return Client instance.
@@ -48,9 +109,23 @@ public class ClientBuilder {
 	public Client build() throws SSLException {
 		Preconditions.checkState(target != null, "please configure etcd server endpoints before build.");
 
-		var channel = NettyChannelBuilder.forTarget(target)
-				.sslContext(GrpcSslContexts.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build())
-				.build();
-		return new ClientImpl(channel);
+		NettyChannelBuilder channelBuilder = NettyChannelBuilder.forTarget(target);
+
+		if (negotiationType != null) {
+			channelBuilder.negotiationType(negotiationType);
+		}
+		if (insecureSkipTLSVerify) {
+			channelBuilder.sslContext(GrpcSslContexts.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build());
+		}
+
+		if (connectTimeout != null) {
+			channelBuilder.withOption(ChannelOption.CONNECT_TIMEOUT_MILLIS, (int) connectTimeout.toMillis());
+		}
+
+		if (interceptors != null) {
+			channelBuilder.intercept(interceptors);
+		}
+
+		return new ClientImpl(channelBuilder.build());
 	}
 }
